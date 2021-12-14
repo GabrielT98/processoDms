5aa0e242433017e5b56e9e04cb7b12fcb26635b9
tamiozzo <tamiozzo98@icloud.com>
2021-12-14 18:42:37 -0300

    criando a classe visitante

---
+++ application/model/entily/visitante.py
@@ -0,0 +1,21 @@
+class Visitante():
+    def __init__(self):
+        self.__nome = None
+        self.__email = None
+        self.__senha = None
+
+    def setNome(self,nome: str):
+        self.__nome = nome
+
+    def setEmail(self,email: str):
+        self.__email = email
+
+    def setSenha(self,senha:str):
+        self.__senha = senha
+
+    def getNome(self):
+        return self.__nome
+    def getEmail(self):
+        return self.__email
+    def getSenha(self):
+        return self.__senha
